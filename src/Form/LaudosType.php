c8e777271455ff4848643766a28eca60e458fea5
Vinícius Vasconcelos Ferreira <vinicius.ferreira@cofendf.cofen.gov.br>
2019-11-18 15:57:27 -0300

    atualização layouts individuais

--- src/Form/LaudosType.php
+++ src/Form/LaudosType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Laudos;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +14,23 @@ class LaudosType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('dsLaudo')
-            ->add('data')
-            ->add('exames')
-        ;
+            ->add('dsLaudo', null, array(
+                'label' => 'Descrição'
+            ))
+            ->add('data', DateType::class, array(
+                'widget' => 'single_text',
+                'format' => 'dd/MM/yyyy',
+                'attr' => array(
+                    'class' => 'datepicker'
+                )
+            ))
+            ->add('exames', ChoiceType::class, array(
+                'placeholder' => '-- Selecione --',
+                'multiple' => false,
+                'attr' => array(
+                    'class' => 'select2'
+                )
+            ));
     }
 
     public function configureOptions(OptionsResolver $resolver)
